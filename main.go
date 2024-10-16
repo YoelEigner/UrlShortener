040c38f3ede3d55f5b45268111eee2d928a3c15d
yoeleigner <joele8323@gmail.com>
2024-10-16 14:53:13 +0200

    add handler for vercel

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/bson"
@@ -16,22 +17,45 @@ var dbName = "shorturls"
 var collectionName = "shorturls"
 var client *mongo.Client
 
-func initMongoClient() {
+func initMongoClient() error {
 	godotenv.Load()
+
 	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
 	db_pass := os.Getenv("DB_PASS")
 	cluster_name := os.Getenv("CLUSTER_NAME")
 	opts := options.Client().ApplyURI("mongodb+srv://yoel:" + db_pass + "@" + cluster_name + "/?retryWrites=true&w=majority&appName=Cluster0").SetServerAPIOptions(serverAPI)
+
 	var err error
 	client, err = mongo.Connect(context.TODO(), opts)
 	if err != nil {
-		panic(err)
+		return err
 	}
 
 	if err := client.Database(dbName).RunCommand(context.TODO(), bson.D{{Key: "ping", Value: 1}}).Err(); err != nil {
-		panic(err)
+		return err
 	}
-	fmt.Println("successfully connected to MongoDB!")
+	fmt.Println("Successfully connected to MongoDB!")
+	return nil
+}
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	if err := initMongoClient(); err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	switch r.URL.Path {
+	case "/":
+		HandleHome(w, r)
+	case "/shorten":
+		HandleShorten(w, r)
+	default:
+		if strings.HasPrefix(r.URL.Path, "/short/") {
+			HandleRedirect(w, r)
+			return
+		}
+		http.NotFound(w, r)
+	}
 }
 
 func main() {
